eaeb97eb02b62525b1f1a08db7e794c3ad345730
primitivecodes <p.ufitinema@alustudent.com>
2026-04-01 14:55:02 +0200

    Add Submissions and Certificates routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 import Modules from './components/Modules';
 import Payments from './components/Payments';
 import Portfolio from './components/Portfolio';
+import Submissions from './components/Submissions';
+import Certificates from './components/Certificates';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* Protected routes */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/modules" element={<Modules />} />
         <Route path="/payments" element={<Payments />} />
         <Route path="/portfolio" element={<Portfolio />} />
+
+        {/* Admin routes */}
+        <Route path="/submissions" element={<Submissions />} />
+        <Route path="/certificates" element={<Certificates />} />
       </Routes>
     </Router>
   );
