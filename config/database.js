c4bfac3ae6448cb640a6b519cbef7ef0080e3df0
primitivecodes <p.ufitinema@alustudent.com>
2026-04-01 14:25:11 +0200

    Delete config directory

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: process.env.DB_STORAGE || './database.sqlite',
-  logging: false
-});
-
-module.exports = { sequelize };
